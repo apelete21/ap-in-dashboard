d5822902d09c0df773aff61e19149ca459939375
Apelete <adzohonouapelete@gmail.com>
2023-03-16 12:10:55 +0000

    Message component created

---
+++ src/components/Message.jsx
@@ -0,0 +1,24 @@
+import React, { useContext } from "react";
+import { AppContext } from "../Contexts/AppContext";
+
+const Message = () => {
+  const { quotedelStatus } = useContext(AppContext);
+  if (!quotedelStatus)
+    return (
+      <div
+        style={{
+          position: "fixed",
+          bottom: 0,
+          textAlign: "center",
+          color: "gray",
+          background: "white",
+          paddingBlock: "0.5rem",
+        }}
+      >
+        {" "}
+        {quotedelStatus}{" "}
+      </div>
+    );
+};
+
+export default Message;
