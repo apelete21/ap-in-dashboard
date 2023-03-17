f2defa80c8b130c1e2d511ff4f68a9b0c8d4d0ff
Apelete <adzohonouapelete@gmail.com>
2023-03-17 16:57:02 +0000

    error component created

---
+++ src/components/Error/ErrorBoundary.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+
+export default class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+  componentDidCatch(error, errorInfo) {
+    console.log(error, errorInfo);
+  }
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div
+          style={{
+            height: "100%",
+            display: "flex",
+            padding: "1rem",
+            alignContent: "center",
+            alignItems: "center",
+            margin: "0 auto",
+            justifyContent: "center",
+          }}
+        >
+          <h2
+            style={{
+              width: "40%",
+              margin: "0 auto",
+            }}
+          >
+            Oups, an error occured...!
+          </h2>
+        </div>
+      );
+    }
+    return this.props.children;
+  }
+}
