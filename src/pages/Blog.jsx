44c9ff6d73370c8614bf05827b0e85b5794ac76d
CoderZ <adzohonouapelete@gmail.com>
2023-05-16 16:11:17 +0000

    blog posts list

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,7 +1,48 @@
-import React from 'react'
+import React, { useState } from "react";
+import { icons } from "../service/icons";
+import PostCard from "../components/blog/PostCard";
+import { Link } from "react-router-dom";
 
 export default function Blog() {
+  const [posts, setPosts] = useState([1, 2, 3, 4, 5, 6]);
+  const [error, setError] = useState();
   return (
-    <div>Blog</div>
-  )
+    <>
+      <br />
+      <div className="job-list-title">
+        <h1>List of Posts</h1>
+      </div>
+      <br />
+
+      <div className="quotes-search-bar-container">
+        <form>
+          <div className="quotes-search-bar">
+            <input type="text" placeholder="Rechercher" />
+            <button>
+              <img src={icons.searchIcon} alt="search icon" />
+            </button>
+          </div>
+        </form>
+      </div>
+
+      <div className="jobs-available-number">
+        {<p>{posts?.length || 0} Posts available</p>}
+        {error && <p> {error} </p>}
+      </div>
+      <div>
+        {posts?.length !== 0 &&
+          posts?.map((e, i) => {
+            return <PostCard key={i} data={e} />;
+          })}
+      </div>
+      <div className="add-application-btn">
+        <Link className="btn cs_btn btn_secondary" to={"/blog/new_post"}>
+          <span className="">
+            <img src={icons.feather} alt="" />
+          </span>
+          <span>Add a new post</span>
+        </Link>
+      </div>
+    </>
+  );
 }
