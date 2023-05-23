2f7c255b88b8c065e54ce53d54edcf1b7d1ff0ce
CoderZ <adzohonouapelete@gmail.com>
2023-05-23 10:28:44 +0000

    editor

--- src/components/jobTemplate/index.jsx
+++ src/components/jobTemplate/index.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import "./template.css";
 import Section from "./elements/section";
+import moment from "moment";
 
-export default function JobTemplate({ setShowModal }) {
+export default function JobTemplate({ job, setJob, setShowModal }) {
   const [content, setContent] = useState([
     <Section />,
   ]);
@@ -18,24 +19,40 @@ export default function JobTemplate({ setShowModal }) {
     <section className="modal-root">
       <div className="modal-closer" onClick={() => setShowModal(false)} />
       <div className="container">
+        <span
+          className="btn modal-btn btn-closer"
+          onClick={() => setShowModal(false)}
+        >
+          close
+        </span>
+        <span
+          className="btn modal-btn btn-submit"
+          onClick={() => setShowModal(false)}
+        >
+          submit
+        </span>
         <header>
           <div className="fluid-wrapper main-navigation bg_primary">
             <div className="offset-canva job-main-title">
-              <h2 className="section--hero__title php_job">{"job?.title"}</h2>
+              <h2 className="section--hero__title php_job">
+                {job?.title || "any"}
+              </h2>
 
               <div className="job_detail__banner">
                 <div className="job_level_block">
                   <div className="level_title">Seniority Level</div>
-                  <div className="level_year">{"job?.level"}</div>
+                  <div className="level_year">{job?.level || "any"}</div>
                 </div>
                 <div className="job_time_block">
                   <div className="time_title">Employment type</div>
-                  <div className="job_time">{"job?.time"}</div>
+                  <div className="job_time">{job?.time || "any"}</div>
                 </div>
 
                 <div className="job_validity_block">
                   <div className="val_title">Validity</div>
-                  <div className="val_year">{"job?.validity"}</div>
+                  <div className="val_year">
+                    {moment(job?.validity)?.format("ll") || "any"}
+                  </div>
                 </div>
               </div>
             </div>
--- src/pages/AddNewJob.jsx
+++ src/pages/AddNewJob.jsx
@@ -1,15 +1,34 @@
 import React, { useState } from "react";
 import { createPortal } from "react-dom";
-import JobTemplate from "../components/template";
+import JobTemplate from "../components/jobTemplate";
 
 export default function AddNewJob() {
   const [showModal, setShowModal] = useState(false);
+  const [job, setJob] = useState({
+    title: "",
+    category: "",
+    enterprise: "",
+    time: "",
+    level: "",
+    validity: "",
+    lowParagraph: "",
+    details: "",
+  });
+
+  const handleChange = (value) => {
+    setJob({
+      ...job,
+      ...value,
+    });
+    console.log(value);
+  };
+
   return (
     <>
       <>
         {showModal &&
           createPortal(
-            <JobTemplate setShowModal={setShowModal} />,
+            <JobTemplate setShowModal={setShowModal} job={job} setJob={setJob} />,
             document.body
           )}
       </>
@@ -27,12 +46,26 @@ export default function AddNewJob() {
           <form action="">
             <div className="input-job-element">
               <p className="input-element-title">Job title</p>
-              <input type="text" placeholder="Job title" />
+              <input
+                type="text"
+                placeholder="Job title"
+                onChange={(e) => handleChange({ title: e.target?.value })}
+              />
+            </div>
+            <div className="input-job-element">
+              <p className="input-element-title">Enterprise Name</p>
+              <input
+                type="text"
+                placeholder="Enterprise Name"
+                onChange={(e) => handleChange({ enterprise: e.target?.value })}
+              />
             </div>
             <div className="input-job-element">
               <p className="input-element-title">Select a category</p>
-              <select name="" id="">
-                <option value="" default>
+              <select
+                onChange={(e) => handleChange({ category: e.target?.value })}
+              >
+                <option value={null} defaultChecked>
                   Select a category
                 </option>
                 <option value="Process improvment">Process improvement</option>
@@ -47,19 +80,17 @@ export default function AddNewJob() {
             </div>
             <div className="input-job-element">
               <p className="input-element-title">Employment type</p>
-              <select name="" id="">
-                <option defaultChecked disabled>
-                  Select a type
-                </option>
+              <select onChange={(e) => handleChange({ time: e.target?.value })}>
+                <option defaultChecked>Select a type</option>
                 <option value="Entry Level">Full Time</option>
               </select>
             </div>
             <div className="input-job-element">
               <p className="input-element-title">Seniority Level</p>
-              <select name="" id="">
-                <option defaultChecked disabled>
-                  Select a level
-                </option>
+              <select
+                onChange={(e) => handleChange({ level: e.target?.value })}
+              >
+                <option defaultChecked>Select a level</option>
                 <option value="Entry Level">Entry Level</option>
                 <option value="Intermediate">Intermediate</option>
                 <option value="Seniority Level">Seniority Level</option>
@@ -67,7 +98,20 @@ export default function AddNewJob() {
             </div>
             <div className="input-job-element">
               <p className="input-element-title">Validity</p>
-              <input type="date" />
+              <input
+                type="date"
+                onChange={(e) =>
+                  handleChange({ validity: e?.target?.valueAsDate })
+                }
+              />
+            </div>
+            <div className="input-job-element">
+              <p className="input-element-title">Description</p>
+              <input
+                type="text"
+                placeholder="Here goes the description..."
+                onChange={(e) => handleChange({ lowParagraph: e.target?.value })}
+              />
             </div>
           </form>
         </div>
