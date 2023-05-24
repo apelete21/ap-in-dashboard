55b3cfe0d98e89c54b9cfa9fb9c5fef572ffd062
CoderZ <adzohonouapelete@gmail.com>
2023-05-24 14:24:58 +0000

    blog

--- src/components/template/elements/subtitle.jsx
+++ src/components/template/elements/subtitle.jsx
@@ -1,16 +1,18 @@
 import random from "../../../service/random";
 import { EditBtn } from "./button";
 import { modifyElement, removeElement } from "../methods";
+import HoverEdit from "./hoverWrapper";
 
 export default function Subtitle() {
   let subTId = "st-" + random();
+  let subTCont = "stc-" + random();
   return (
-    <h4 className={`h4 ${subTId}`}>
-      Here goes the Subtitle...
+    <HoverEdit className={`${subTCont}`}>
+      <h4 className={`h4 ${subTId}`}>Here goes the Subtitle...</h4>
       <EditBtn
         editFunc={() => modifyElement(subTId)}
-        deleteFunc={() => removeElement(subTId)}
+        deleteFunc={() => removeElement(subTCont)}
       />
-    </h4>
+    </HoverEdit>
   );
 }
