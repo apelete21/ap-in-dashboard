3f67779aefdab6a45eb5338598c60e16b53aee11
apelete21 <adzohonouapelete@gmail.com>
2023-06-16 06:03:01 +0000

    blog_editor

--- src/components/template/elements/blog/img.jsx
+++ src/components/template/elements/blog/img.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import random from "../../../../service/random";
+import { modifyElement, removeElement } from "../../methods";
 
 
 export function BannerImg() {
@@ -17,24 +19,54 @@ export function BannerImg() {
 
 export function ImgContainer() {
   const [images, setImages] = useState([<ImageCard />]);
+  const captionID = "capId-" + random();
+  const contID = "contId-" + random();
   const addImgSection = () => {
     setImages([...images, <ImageCard />])
   }
+
+  const modifyCaption = () => {
+    modifyElement(captionID)
+  }
+
+  const removeImageContainer = () => {
+    removeElement(contID)
+  }
+
   return (
     <>
-      <div class="illustration-img">
-        <button onClick={addImgSection}>add second image</button>
+      <div class={`illustration-img ${contID}`}>
+        <div className="image-btn-container">
+          <button className="image-btn" onClick={addImgSection}>
+            add image
+          </button>
+          <button className="image-btn" onClick={modifyCaption}>
+            modify caption
+          </button>
+          <button className="image-btn" onClick={removeImageContainer}>
+            remove
+          </button>
+        </div>
         <div class="illustration-img--container dflex">{images}</div>
-        <p class="img-info">Spring in Japan lasts from about mid</p>
+        <p class={`img-info ${captionID}`}>
+          Spring in Japan lasts from about mid
+        </p>
       </div>
     </>
   );
 }
 
 export function ImageCard() {
+  const imageId = "img--" + random()
+  const deleteImage = () => {
+      removeElement(imageId)
+  }
   return (
     <>
-      <div class="illustration-image">
+      <div class={`illustration-image ${imageId}`}>
+        <button className="image-btn-remove" onClick={deleteImage}>
+          X
+        </button>
         <img
           src="https://appealofinnovation.com/assets/media/images/news/news-two.png"
           alt=""
