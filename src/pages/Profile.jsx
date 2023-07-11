0773074d9cbf9e2aa636a592ac09c46771729128
apelete21 <adzohonouapelete@gmail.com>
2023-07-11 21:47:53 +0000

    profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -6,10 +6,10 @@ export default function Profile() {
   return (
     <>
       <Helmet>
-        <title>Profile</title>
+        <title>Profile | AP'IN</title>
       </Helmet>
       <div className="profile">
-        <div className="pictures">
+        <div className="profile-header">
           <div className="cover-picture">
             <img src={bgs.bannerImg} alt="cover-picture" />
           </div>
@@ -19,6 +19,27 @@ export default function Profile() {
           <div className="user-info wpad">
             <h2 className="info-name">Marc Doe</h2>
             <p className="info-email">marcdoe@gmail.com</p>
+          </div>
+        </div>
+        <div className="profile-body">
+          <div className="side-pane">
+            <div className="user-details">
+              <div className="user-details-option">
+                <p>Pays:</p>
+                <p>Sénégal</p>
+              </div>
+              <div className="user-details-option">
+                <p>Addresse:</p>
+                <p>Angré 7eme tranche, Abidjan, Cote d'ivoire</p>
+              </div>
+              <div className="user-details-option">
+                <p>Téléphone:</p>
+                <p>+228 7886325636</p>
+              </div>
+            </div>
+          </div>
+          <div className="user-main">
+            
           </div>
         </div>
       </div>
