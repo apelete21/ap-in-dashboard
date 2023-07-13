e12648c3f64a794d22afcb5c0e21018364edad2b
apelete21 <adzohonouapelete@gmail.com>
2023-07-13 09:12:15 +0000

    profile

--- src/components/profile/add.jsx
+++ src/components/profile/add.jsx
@@ -1,7 +1,49 @@
-import React from 'react'
+import React from "react";
+import { teams } from "../../service/icons";
 
 export default function AddProfile() {
   return (
-    <div>AddProfile</div>
-  )
+    <>
+      <div className="tab-me">
+        <div className="profile-image">
+          <div className="image-show">
+            <img src={teams.Roger} alt="profile" />
+          </div>
+          <div className="picture-actions">
+            <span>Charger une photo</span>
+            <span>Retirer la photo</span>
+          </div>
+        </div>
+        <div className="user-datas">
+          <div className="user-data-item">
+            <h3>Noms et Prénoms</h3>
+            <input type="text" placeholder="Noms et Prénoms" />
+          </div>
+          <div className="user-data-item">
+            <h3>Téléphone</h3>
+            <input type="text" placeholder="Téléphone" />
+          </div>
+          <div className="user-data-item">
+            <h3>Pays</h3>
+            <input type="text" placeholder="Pays" />
+          </div>
+          <div className="user-data-item">
+            <h3>Email</h3>
+            <input type="email" autoComplete={false} placeholder="Email" />
+          </div>
+          <div className="user-data-item">
+            <h3>Password</h3>
+            <input
+              type="password"
+              autoComplete={false}
+              placeholder="password"
+            />
+          </div>
+        </div>
+        <div className="updater-button">
+          <span>Enrégistrer</span>
+        </div>
+      </div>
+    </>
+  );
 }
