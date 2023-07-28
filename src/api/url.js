cd849d7a705f489b58db0105d67b29a751589235
ADZOHONOU K. Apelete <87383047+apelete21@users.noreply.github.com>
2023-07-28 20:18:03 +0000

    Update url.js

--- src/api/url.js
+++ src/api/url.js
@@ -3,5 +3,5 @@
 | Exporting api url to connect to.
 |--------------------------------------------------
 */
-// export const baseUrl = "https://ap-in-api-production.up.railway.app/api/v1"
-export const baseUrl = "http://localhost:5000/api/v1";
+export const baseUrl = "https://ap-in-api-production.up.railway.app/api/v1"
+// export const baseUrl = "http://localhost:5000/api/v1";
