fa5400150da931a065a06b43da73576fe68c0e2c
ADZOHONOU K. Apelete <87383047+apelete21@users.noreply.github.com>
2024-01-25 16:14:47 +0000

    Update url.js

--- src/api/url.js
+++ src/api/url.js
@@ -4,7 +4,7 @@
 |--------------------------------------------------
 */
 
-export const baseUrl = "https://ap-in-api-flax.vercel.app/api/v1"
+export const baseUrl = "https://apin-oabu.onrender.com/api/v1"
 // export const baseUrl = "http://localhost:5000/api/v1";
 export const appUrl = "https://www.appealofinnovation.com/";
 // export const appUrl = "http://localhost:3001";
